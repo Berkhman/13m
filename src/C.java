f1a199d06ea910aa7aeacf3c51adc3664b37653a
Berkhman <memeboxermark@gmail.com>
2017-03-13 11:05:43 +0300

    FC

---
+++ src/C.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by berhmaneyu.18 on 13.03.2017.
+ */
+public class C {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        int a;
+        for(int i = 0; i < n; i++){
+            a = scanner.nextInt();
+            sum = sum + a;
+        }
+        int d = sum / n;
+        System.out.print(d);
+    }
+}
